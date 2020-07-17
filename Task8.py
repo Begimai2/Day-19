bcb51c9e37d411a844607c78c2205ffa48e53626
Begimai2 <begi.ab@mail.ru>
2020-07-17 14:34:15 +0600

    Day-19

---
+++ Task8.py
@@ -0,0 +1,11 @@
+suitcase = [ ]
+suitcase.append('food')
+suitcase.append('clothes')
+suitcase.append('hat')
+suitcase.append('shoes')
+suitcase.append('underwear')
+
+
+suitcase.pop(4)
+suitcase.insert(0, 'toothbrush')
+print(suitcase) 
